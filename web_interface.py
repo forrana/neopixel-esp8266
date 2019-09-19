61331a126675e5ff1c76f9e11a2a3957335812d2
Aliaksei Kuchyn <forrana@gmail.com>
2019-09-20 00:02:53 +0300

    Add ability to change color from the web interface

--- web_interface.py
+++ web_interface.py
@@ -21,13 +21,11 @@ class Server:
 
         poller = select.poll()
         poller.register(s, select.POLLIN)
-        client_id = 1  # For user feedback
         while True:
             res = poller.poll(1)  # 1ms block
             if res:  # Only s_sock is polled
                 cl, addr = s.accept()
                 loop.create_task(self.run_client(cl, addr))
-                client_id += 1
             await asyncio.sleep_ms(200)
 
     async def run_client(self, sock, cid):
@@ -42,15 +40,25 @@ class Server:
                     raise OSError
                 print('Received {} from client {}'.format(res, cid))
                 if 'GET /?' in res:
-                    regex1 = re.compile('(programm[a-z,=,0-9]+)')
-                    new_programm_number = regex1.search(res)
+                    utf8Res = res.decode('UTF-8')
+                    program_regex = re.compile('(programm[a-z,=,0-9]+)')
+                    color_regex = re.compile('(color[a-z,=,0-9,%]+)')
+                    new_programm_number = program_regex.search(utf8Res)
+                    new_color = color_regex.search(utf8Res)
                     if new_programm_number is not None:
-                        global_vars.PROGRAMM_NUMBER = int(new_programm_number.group(0).decode('UTF-8').split('=')[1])
+                        global_vars.PROGRAMM_NUMBER = int(new_programm_number.group(0).split('=')[1])
                         print(global_vars.PROGRAMM_NUMBER)
+                    if new_color is not None:
+                        new_color = new_color.group(0).split('=')[1]
+                        new_color = new_color.replace("%23", "")
+                        color_array_hex = [new_color[i:i+2] for i in range(0, len(new_color), 2)]
+                        color_array_hex.append('00')
+                        color_array_decimal = map(lambda  x:int(x,16), color_array_hex)
+                        global_vars.LED_COLOR = tuple(color_array_decimal)
                 if not res or res == b'\r\n':
                    break        
             response = html
-            await swriter.awrite(response)  # Echo back
+            await swriter.awrite(response)
             print('Client {} disconnect.'.format(cid))
             sock.close()
             self.socks.remove(sock)
